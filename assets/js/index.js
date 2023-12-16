cd17b72df2c015813ec7e212249029d09c3802f0
WebAlexB <belousalek2@gmail.com>
2023-12-16 19:33:24 +0200

    fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,15 +2,14 @@
 
 const array = [1, '2', 5, 3, '5', 4, 5, 6, '7'];
 
-// Результат: [1, '2', 3, '5', 4, 6, '7']
-
 function removeElement(array, item) {
-    const index = array.indexOf(item);
-    if (index > -1) {
-        array.splice(index, 1);
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === item) {
+            array.splice(i, 1);
+        }
     }
-    return array;
 }
 
 removeElement(array, 5);
-console.log(removeElement(array, 5));
+console.log(array);
+
